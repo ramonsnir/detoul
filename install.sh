5c37bc98ccfd9e354097c6faf903b4af2bf02fd2
Ramon Snir <ramon@dynamicyield.com>
2015-01-27 13:44:04 +0200

    added a simple(!) install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+DETOUL_DIR="$(cd "$( dirname "$SOURCE" )" && pwd -P)"
+
+(
+  echo ""
+  echo "# load detoul and its auto-completion script"
+  echo "export PATH=\"\$PATH:$DETOUL_DIR\""
+  echo "source \"$DETOUL_DIR/bash-completion/detoul\""
+) >> ~/.bashrc
